eba6939121e340509c999c5fd15ff2e2c86feeec
lihuohuo1 <1154952241@qq.com>
2018-12-12 23:39:52 -0800

    a

---
+++ Python1810Axf/app/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import Wheel
+
+
+def home(request):
+
+    # 获取轮播图数据
+    wheels = Wheel.objects.all()
+
+
+    data = {
+        'wheels':wheels
+    }
+
+    return render(request, 'home/home.html', context=data)
+
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def mine(request):
+    return render(request, 'mine/mine.html')
